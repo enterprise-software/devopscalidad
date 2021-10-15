181a429ad5c52df241b89d48c5593809f057d777
Enrique de Jesús Durango Rivera <Edurango@intergrupo.com>
2021-10-15 16:54:06 -0500

    Initial commit

---
+++ src/main/java/co/edu/udem/loscalidosos/devopscalidad/DevopscalidadApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udem.loscalidosos.devopscalidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopscalidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopscalidadApplication.class, args);
+	}
+
+}
